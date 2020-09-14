afc716331165e8450a628ee583c89cfb2adb4004
aswinjayaji <aswinjayaji@cet.ac.in>
2020-09-14 19:44:06 +0530

    updated

--- backend/djreact/articles/api/serializers.py
+++ backend/djreact/articles/api/serializers.py
@@ -4,4 +4,4 @@ from ..models import Article
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
-        fields=('title','contents')
+        fields=('title','content')
--- backend/djreact/articles/api/urls.py
+++ backend/djreact/articles/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import ArticleListView,ArticleDetailView
 
-url=[
+urlpatterns=[
     path('',ArticleListView.as_view()),
     path('<pk>',ArticleDetailView.as_view())
 ]
