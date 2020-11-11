e79997ce2d0020475f80091435beebbd68389255
aswinjayaji <aswinjayaji@cet.ac.in>
2020-11-12 00:28:57 +0530

    updated

--- frontend/gui/src/components/Article.js
+++ frontend/gui/src/components/Article.js
@@ -3,19 +3,6 @@ import { List, Avatar, Space } from 'antd';
 import { MessageOutlined, LikeOutlined, StarOutlined } from '@ant-design/icons';
 
 
-const listData = [];
-for (let i = 0; i < 23; i++) {
-  listData.push({
-    href: 'https://ant.design',
-    title: `ant design part ${i}`,
-    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-    description:
-      'Ant Design, a design language for background applications, is refined by Ant UED Team.',
-    content:
-      'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
-  });
-}
-
 const IconText = ({ icon, text }) => (
   <Space>
     {React.createElement(icon)}
@@ -23,7 +10,7 @@ const IconText = ({ icon, text }) => (
   </Space>
 );
 
-const Article =(props) => {
+const Articles =(props) => {
     return(
         <List
         itemLayout="vertical"
@@ -34,7 +21,7 @@ const Article =(props) => {
           },
           pageSize: 3,
         }}
-        dataSource={listData}
+        dataSource={props.data}
         footer={
           <div>
             <b>ant design</b> footer part
@@ -68,4 +55,4 @@ const Article =(props) => {
     );
 }
     
-export default Article;
+export default Articles;
